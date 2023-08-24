715dfd2a85accd8c5cbe825c56d88b1ea7492fae
taeook <aksehfdl65@naver.com>
2023-08-24 17:14:24 +0900

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h2>firebase 배포하기</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+
+export default {
+/*   name: 'HomeView',
+  component:{
+    HeeloWorld
+  } */
+};
+</script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  height:90vh;
+  align-items: center;
+}
+</style>
